8747287d103fb637f9ab34f558c5bc0c6fa26cae
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-04 15:50:20 +0300

    edit Chart component

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable no-undef */
+import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { createChart } from 'lightweight-charts';
 
-const Chart = (data) => {
-  const chart = createChart(document.getElementById('denemex'), {
-    width: 1077,
-    height: 274,
-  });
+const Chart = ({ data }) => {
+  useEffect(() => {
+    document.getElementById('chart').innerHTML = '';
+    const chart = createChart(document.getElementById('chart'), {
+      width: 1077,
+      height: 274,
+    });
 
-  const areaSeries = chart.addAreaSeries({
-    topColor: 'rgba(33, 150, 243, 0.56)',
-    bottomColor: 'rgba(33, 150, 243, 0.04)',
-    lineColor: 'rgba(33, 150, 243, 1)',
-    lineWidth: 2,
-  });
+    const areaSeries = chart.addAreaSeries({
+      topColor: 'rgba(33, 150, 243, 0.56)',
+      bottomColor: 'rgba(33, 150, 243, 0.04)',
+      lineColor: 'rgba(33, 150, 243, 1)',
+      lineWidth: 2,
+    });
 
-  areaSeries.setData([...data]);
+    areaSeries.setData([...data]);
+  }, [data]);
+
+  return <div id="chart" />;
+};
+
+Chart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
 };
 
 export default Chart;
