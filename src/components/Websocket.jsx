c89a6db204b0ba6191305f5cd65d5ebeab8f69e4
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-08 13:49:52 +0300

    create a component for websocket

---
+++ src/components/Websocket.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState, useContext } from 'react';
+
+import useWebSocket from 'react-use-websocket';
+
+import SelectSymbolContext from '../context/SelectSymbolContext';
+import LiveDataContext from '../context/LiveDataContext';
+
+function Websocket() {
+  const { selectedSymbol } = useContext(SelectSymbolContext);
+  const lowerCased = selectedSymbol.toLowerCase();
+
+  const [socketUrl, setSocketUrl] = useState(
+    `wss://stream.binance.com:9443/ws/${lowerCased}@ticker`,
+  );
+
+  useEffect(() => {
+    setSocketUrl(
+      `wss://stream.binance.com:9443/ws/${lowerCased}@ticker`,
+    );
+  }, [selectedSymbol]);
+
+  const { lastMessage } = useWebSocket(socketUrl, {
+    shouldReconnect: () => true,
+  });
+
+  const { setLiveData } = useContext(LiveDataContext);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      const parsedData = JSON.parse(lastMessage.data);
+
+      setLiveData(parsedData);
+    }
+  }, [lastMessage]);
+
+  return null;
+}
+
+export default Websocket;
