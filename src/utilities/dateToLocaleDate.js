80d1be86a19cdb04852decb449e48bef306e5fa6
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-08 13:15:01 +0300

    add style option to month

--- src/utilities/dateToLocaleDate.js
+++ src/utilities/dateToLocaleDate.js
@@ -1,11 +1,13 @@
 // convert unix date to yyyy-month-dd format
-function dateToLocaleDate(unixDate) {
+function dateToLocaleDate(unixDate, isNumericMonth) {
   const date = new Date(unixDate);
+
+  const option = isNumericMonth ? 'numeric' : 'long';
 
   const convertedDate = `${date.toLocaleDateString('en-US', {
     year: 'numeric',
   })}-${date.toLocaleDateString('en-US', {
-    month: 'long',
+    month: option,
   })}-${date.toLocaleDateString('en-US', { day: 'numeric' })}`;
 
   return convertedDate;
