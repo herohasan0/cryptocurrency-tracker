d348a9985e11f2beb73d0fbec377a05461de40a6
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-08 15:13:44 +0300

    add spinner to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Flex, Heading, Text, Spinner } from '@chakra-ui/react';
 
 function Card({ color, title, text }) {
   return (
@@ -16,7 +17,11 @@ function Card({ color, title, text }) {
       mr="27.5px"
     >
       <Heading fontSize="48px" fontWeight="300">
-        {title}
+        {title === 'NaN' || title === 'Invalid Date' ? (
+          <Spinner size="md" />
+        ) : (
+          title
+        )}
       </Heading>
       <Text fontSize="15px" mt="2px">
         {text}
